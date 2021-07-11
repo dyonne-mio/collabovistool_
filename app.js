2d14917291cca5589793e57d0c967604796e64cc
Dyonne <dyonne-mio@users.noreply.github.com>
2021-07-11 16:32:20 +0800

    Updated with most recent version of CollaboVisTool

--- app.js
+++ app.js
@@ -20,64 +20,58 @@ const driver = neo4j.driver(
 
 console.log(`Database running at ${neo4jUri}`)
 
-function searchAuthors(queryString) {
-  const session = driver.session({database: database});
-  return session.readTransaction((tx) =>
-      tx.run('MATCH (author:Author) \
-      WHERE author.full_name =~ $full_name \
-      RETURN author',
-      {full_name: '(?i).*' + queryString + '.*'})
-    )
-    .then(result => {
-      return result.records.map(record => {
-        return new Author(record.get('author'));
-      });
-    })
-    .catch(error => {
-      throw error;
-    })
-    .finally(() => {
-      return session.close();
-    });
-}
-
-function getAuthor(full_name) {
-  const session = driver.session({database: database});
-  return session.readTransaction((tx) =>
-      tx.run("MATCH (author:Author {full_name:$full_name}) \
-      OPTIONAL MATCH (author)<-[r]-(paper:Paper) \
-      RETURN author.full_name AS full_name, \
-      collect([paper.paper_title, \
-           head(split(toLower(type(r)), '_')), r.abstract]) AS abstract \
-      LIMIT 1", {full_name}))
-    .then(result => {
-      if (_.isEmpty(result.records))
-        return null;
-
-      const record = result.records[0];
-      return new MovieCast(record.get('full_name'), record.get('abstract'));
-    })
-    .catch(error => {
-      throw error;
-    })
-    .finally(() => {
-      return session.close();
-    });
-}
-
-function getGraph() {
-  const session = driver.session({database: database});
-  
-  return session.readTransaction(tx =>
-    tx.run("MATCH (a1:Author)-[r:COAUTHORED_WITH]->(a2:Author) \
+function searchYear(queryYear) {
+	cypher_string = 'MATCH (a1:Author)-[r:COAUTHORED_WITH]->(a2:Author) \
+	WHERE r.year <= "' + queryYear + '"\
     RETURN a1.auth_name AS name1, a2.auth_name AS name2, ID(a1) AS id1, ID(a2) AS id2, \
 	a1.auth_papers AS paper1, a2.auth_papers AS paper2, \
 	a1.degree_centrality AS deg1, a2.degree_centrality AS deg2, \
 	a1.betweenness_centrality AS bet1, a2.betweenness_centrality AS bet2, \
 	a1.closeness_centrality AS clo1, a2.closeness_centrality AS clo2, \
 	a1.eigenvector_centrality AS eig1, a2.eigenvector_centrality AS eig2, \
-	a1.louvain_community AS lou1, a2.louvain_community AS lou2 \
-    LIMIT $limit", {limit: neo4j.int(1000)})) // 60 000
+	a1.louvain_community AS lou1, a2.louvain_community AS lou2, \
+	a1.aff_id AS afid1, a2.aff_id AS afid2 \
+    LIMIT $limit'
+	
+	return generateNodesandEdges(cypher_string);
+}
+
+function searchAuthors(queryString) {
+    cypher_string = 'MATCH (a1:Author)-[r:COAUTHORED_WITH]->(a2:Author) \
+	WHERE a1.auth_name CONTAINS "' + queryString + '"\
+    RETURN a1.auth_name AS name1, a2.auth_name AS name2, ID(a1) AS id1, ID(a2) AS id2, \
+	a1.auth_papers AS paper1, a2.auth_papers AS paper2, \
+	a1.degree_centrality AS deg1, a2.degree_centrality AS deg2, \
+	a1.betweenness_centrality AS bet1, a2.betweenness_centrality AS bet2, \
+	a1.closeness_centrality AS clo1, a2.closeness_centrality AS clo2, \
+	a1.eigenvector_centrality AS eig1, a2.eigenvector_centrality AS eig2, \
+	a1.louvain_community AS lou1, a2.louvain_community AS lou2, \
+	a1.aff_id AS afid1, a2.aff_id AS afid2 \
+    LIMIT $limit'
+	
+	return generateNodesandEdges(cypher_string);
+}
+
+function getGraph() {	
+	cypher_string = "MATCH (a1:Author)-[r:COAUTHORED_WITH]->(a2:Author) \
+    RETURN a1.auth_name AS name1, a2.auth_name AS name2, ID(a1) AS id1, ID(a2) AS id2, \
+	a1.auth_papers AS paper1, a2.auth_papers AS paper2, \
+	a1.degree_centrality AS deg1, a2.degree_centrality AS deg2, \
+	a1.betweenness_centrality AS bet1, a2.betweenness_centrality AS bet2, \
+	a1.closeness_centrality AS clo1, a2.closeness_centrality AS clo2, \
+	a1.eigenvector_centrality AS eig1, a2.eigenvector_centrality AS eig2, \
+	a1.louvain_community AS lou1, a2.louvain_community AS lou2, \
+	a1.aff_id AS afid1, a2.aff_id AS afid2 \
+    LIMIT $limit"
+	
+	return generateNodesandEdges(cypher_string);
+}
+
+function generateNodesandEdges(cypher_string){
+  const session = driver.session({database: database});
+  
+  return session.readTransaction(tx =>
+    tx.run(cypher_string, {limit: neo4j.int(1000)})) // 60 000
 	.then(results => {
 	  var nodes = [], links = [], names = [];
 	  var i = 0;
@@ -87,7 +81,7 @@ function getGraph() {
 		   //var node1 = Object.create({}, { id: { value: res.get('id1').low, enumerable: true}, name: { value: res.get('name1'), enumerable: true}, label: { value: 'author', enumerable: true} });
 		   //nodes.push(node1);
 		   nodes.push({id: res.get('id1').low, name: res.get('name1'), label: 'author',
-					   p_count: res.get('paper1'), 
+					   p_count: res.get('paper1'), afid: res.get('afid1'),
 					   d_cen: res.get('deg1'), b_cen: res.get('bet1'), c_cen: res.get('clo1'), 
 					   e_cen: res.get('eig1'), l_com: res.get('lou1')});
 		   names.push(res.get('name1'));
@@ -96,14 +90,11 @@ function getGraph() {
 		   //var node2 = Object.create({}, { id: { value: res.get('id2').low, enumerable: true}, name: { value: res.get('name2'), enumerable: true}, label: { value: 'author', enumerable: true} });
 		   //nodes.push(node2);
 		   nodes.push({id: res.get('id2').low, name: res.get('name2'), label: 'author',
-					   p_count: res.get('paper2'), 
+					   p_count: res.get('paper2'), afid: res.get('afid2'),
 					   d_cen: res.get('deg2'), b_cen: res.get('bet2'), c_cen: res.get('clo2'), 
 					   e_cen: res.get('eig2'), l_com: res.get('lou2')});
 		   names.push(res.get('name2'));
 		}
-		
-		//nodes.push({name: res.get('name1'), label: 'author'});
-		//nodes.push({name: res.get('name2'), label: 'author'});
 		
 		const author1 = {name: res.get('name2'), label: 'author'};
         let target = _.findIndex(nodes, author1);
@@ -118,16 +109,7 @@ function getGraph() {
 			source = i;
 			i++;
 		}
-		links.push({source, target});
-
-		//const target = res.get('id2').low;
-		//const source = res.get('id1').low;
-		
-		
-		//var connect = {source, target}
-		//var connect = Object.create({}, { source: { value: source, enumerable: true}, target: { value: target, enumerable: true} });
-		//links.push(connect)
-		
+		links.push({source, target});		
       });
       return {nodes, links};
     })
@@ -139,91 +121,6 @@ function getGraph() {
     });
 }
 
-function searchMovies(queryString) {
-  const session = driver.session({database: database});
-  return session.readTransaction((tx) =>
-      tx.run('MATCH (movie:Movie) \
-      WHERE movie.title =~ $title \
-      RETURN movie',
-      {title: '(?i).*' + queryString + '.*'})
-    )
-    .then(result => {
-      return result.records.map(record => {
-        return new Movie(record.get('movie'));
-      });
-    })
-    .catch(error => {
-      throw error;
-    })
-    .finally(() => {
-      return session.close();
-    });
-}
-
-function getMovie(title) {
-  const session = driver.session({database: database});
-  return session.readTransaction((tx) =>
-      tx.run("MATCH (movie:Movie {title:$title}) \
-      OPTIONAL MATCH (movie)<-[r]-(person:Person) \
-      RETURN movie.title AS title, \
-      collect([person.name, \
-           head(split(toLower(type(r)), '_')), r.roles]) AS cast \
-      LIMIT 1", {title}))
-    .then(result => {
-      if (_.isEmpty(result.records))
-        return null;
-
-      const record = result.records[0];
-      return new MovieCast(record.get('title'), record.get('cast'));
-    })
-    .catch(error => {
-      throw error;
-    })
-    .finally(() => {
-      return session.close();
-    });
-}
-
-function getMovieGraph() {
-  const session = driver.session({database: database});
-  return session.readTransaction((tx) =>
-    tx.run('MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) \
-    RETURN m.title AS movie, collect(a.name) AS cast \
-    LIMIT $limit', {limit: neo4j.int(100)}))
-    .then(results => {
-      const nodes = [], rels = [];
-      let i = 0;
-      results.records.forEach(res => {
-        nodes.push({title: res.get('movie'), label: 'movie'});
-        const target = i;
-        i++;
-
-        res.get('cast').forEach(name => {
-          const actor = {title: name, label: 'actor'};
-          let source = _.findIndex(nodes, actor);
-          if (source === -1) {
-            nodes.push(actor);
-            source = i;
-            i++;
-          }
-          rels.push({source, target})
-        })
-      });
-
-      return {nodes, links: rels};
-    })
-    .catch(error => {
-      throw error;
-    })
-    .finally(() => {
-      return session.close();
-    });
-}
-
-exports.searchMovies = searchMovies;
-exports.getMovie = getMovie;
-exports.getMovieGraph = getMovieGraph;
-
 exports.searchAuthors = searchAuthors;
-exports.getAuthor = getAuthor;
+exports.searchYear = searchYear;
 exports.getGraph = getGraph;
--- js/main.js
+++ js/main.js
@@ -7,8 +7,19 @@ import '../js/popper.min.js';
 import '../js/jquery-3.3.1.slim.min.js';
 import '../js/bootstrap.bundle.min.js';
 import '../js/neo4j-web.min.js';
+//import '../js/d3.min.js';
 import teamIcon from '../img/team-member.png';
 import collaboIcon from '../img/collabo-icon.png';
+
+const element = document.getElementById('panzoom')
+const panzoom = Panzoom(element, {
+      // options here
+});
+
+// enable mouse wheel
+const parent = element.parentElement
+parent.addEventListener('wheel', panzoom.zoomWithWheel);
+
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
@@ -16,6 +27,41 @@ output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
+
+  $("#graph").empty();
+  document.getElementById('search-input').value = "";
+  
+  const width = 800, height = 800;
+  const force = d3.layout.force()
+    .charge(-200).linkDistance(30).size([width, height]);
+
+  const svg = d3.select("#graph").append("svg")
+    .attr("width", "100%").attr("height", "100%")
+	.attr("display", "block").attr("margin", "auto")
+	.attr("viewBox", "-600 -600 2100 2100")
+    .attr("pointer-events", "all");
+
+  api
+    .searchYear(this.value)
+    .then(graph => {
+      createNodesandEdges(width, force, svg, graph);
+	  
+	  if (document.getElementById("communities-tab").classList.contains("active")){
+		  var color = d3.scale.category10();
+		  svg.selectAll(".node").style("fill", function(d) { return color(d.l_com); });
+	  }
+	  else if (document.getElementById("affiliations-tab").classList.contains("active")){
+		  var color = d3.scale.category10();
+		  svg.selectAll(".node").style("fill", function(d) { return color(d.afid); });
+	  }
+	  else if (document.getElementById("authors-tab").classList.contains("active")) {
+		  var colorScale = d3.scale.quantize()
+			.range(['#f7fcfd','#e5f5f9', '#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45', '#006d2c', '#00441b'])
+			.domain([0,9]);
+			
+			svg.selectAll(".node").style("fill", function(d) { return colorScale(d.d_cen); });
+	  }
+	});  
 }
 
 var teamImg = document.getElementsByClassName('team');
@@ -31,48 +77,103 @@ const api = require('../app.js');
 
 $(function () {
   renderGraph();
-  search();
 
-  $("#search-addon").submit(e => {
-    e.preventDefault();
+  $("#search-form").submit(e => {
+    console.log("searched!");
+	e.preventDefault();
     search();
   });
 });
 
-$("#search-addon").click(function() {
-	console.log("search");
-	search();
+var divItems = document.getElementsByClassName("radiodiv");
+
+function selected(item) {
+    this.clear();
+    item.style.backgroundColor = 'red';
+}
+
+function clear() {
+    for(var i=0; i < divItems.length; i++) {
+        var item = divItems[i];
+        item.style.backgroundColor = 'white';
+    }
+}
+$("#authors-tab").click(function() {
+	var svg = d3.select("svg");
+		
+	var colorScale = d3.scale.quantize()
+    .range(['#f7fcfd','#e5f5f9', '#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45', '#006d2c', '#00441b'])
+    .domain([0,9]);
+	
+	svg.selectAll(".node").style("fill", function(d) { return colorScale(d.d_cen); });
+  $("#affiliations-tab")
+     .removeClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();
+  $(this)
+     .addClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();
+  $("#communities-tab")
+     .removeClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();  
+})
+
+$("#affiliations-tab").click(function() {
+	var svg = d3.select("svg");
+	
+	var color = d3.scale.category20();
+	svg.selectAll(".node").style("fill", function(d) { return color(d.afid); });
+	
+  $("#authors-tab")
+     .removeClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();
+  $(this)
+     .addClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();
+  $("#communities-tab")
+     .removeClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();   
+})
+
+$("#communities-tab").click(function() {
+	var svg = d3.select("svg");
+	
+	var color = d3.scale.category10();
+	svg.selectAll(".node").style("fill", function(d) { return color(d.l_com); });
+  $("#affiliations-tab")
+     .removeClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();
+  $(this)
+     .addClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();
+  $("#authors-tab")
+     .removeClass("active")
+     // get the nested children and hide
+     .find('ul') 
+     .hide();  
 })
 
 function search() {
-  const query = $("#search-addon").find("input[name=search]").val();
-  api
-    .searchMovies(query)
-    .then(movies => {
-		var elementID = d3.select('svg').selectAll('.node');	
-		//showMovie($(elementID).text());
-		
-		/*
-      const t = $("table#results tbody").empty();
-
-      if (movies) {
-        movies.forEach(movie => {
-          $("<tr><td class='movie'>" + movie.title + "</td><td>" + movie.released + "</td><td>" + movie.tagline + "</td></tr>").appendTo(t)
-            .click(function() {
-              showMovie($(this).find("td.movie").text());
-            })
-        });
-
-        const first = movies[0];
-        if (first) {
-          showMovie(first.title);
-        }
-      }*/
-    });
-}
-
-
-function renderGraph() {
+  const query = $("#search-input").val();
+  $("#graph").empty();
+  slider.value = "2020";
+  output.innerHTML = slider.value;
+  
   const width = 800, height = 800;
   const force = d3.layout.force()
     .charge(-200).linkDistance(30).size([width, height]);
@@ -84,13 +185,55 @@ function renderGraph() {
     .attr("pointer-events", "all");
 
   api
+    .searchAuthors(query)
+    .then(graph => {
+      createNodesandEdges(width, force, svg, graph);
+	  
+	  if (document.getElementById("communities-tab").classList.contains("active")){
+		  var color = d3.scale.category10();
+		  svg.selectAll(".node").style("fill", function(d) { return color(d.l_com); });
+	  }
+	  else if (document.getElementById("affiliations-tab").classList.contains("active")){
+		  var color = d3.scale.category10();
+		  svg.selectAll(".node").style("fill", function(d) { return color(d.afid); });
+	  }
+	  else if (document.getElementById("authors-tab").classList.contains("active")) {
+		  var colorScale = d3.scale.quantize()
+			.range(['#f7fcfd','#e5f5f9', '#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45', '#006d2c', '#00441b'])
+			.domain([0,9]);
+			
+			svg.selectAll(".node").style("fill", function(d) { return colorScale(d.d_cen); });
+	  }
+	});  
+}
+
+function renderGraph() {
+  const width = 800, height = 800;
+  const force = d3.layout.force()
+    .charge(-200).linkDistance(30).size([width, height]);
+
+  const svg = d3.select("#graph").append("svg")
+    .attr("width", "100%").attr("height", "100%")
+	.attr("display", "block").attr("margin", "auto")
+	.attr("viewBox", "-600 -600 2100 2100")
+    .attr("pointer-events", "all");
+  
+  api
     .getGraph()
     .then(graph => {
-		console.log(graph)
-		//Object.values(graph)[1].forEach(value => console.log(JSON.stringify(value.target)))
+		createNodesandEdges(width, force, svg, graph);
+		document.getElementById("authors-tab").click();
+    });
+}
+
+function createNodesandEdges(width, force, svg, graph){
+	console.log(graph)
+	//Object.values(graph)[1].forEach(value => console.log(JSON.stringify(value.target)))
 		
       force.nodes(graph.nodes).links(graph.links).start();
-      const link = svg.selectAll(".link")
+	  
+
+	  const link = svg.selectAll(".link")
         .data(graph.links).enter()
         .append("line").attr("class", "link");
 
@@ -105,13 +248,13 @@ function renderGraph() {
 		})
         .attr("r", 10)
 		.attr("fill", "red")
+		//.style("fill", function(d) { return color(d.l_com); })
 		.on("click", function(d){
 			//var elementID = d3.select(this).attr("class").slice(-1);
 			var nodeID = d3.select('svg').selectAll('.node');
 			var linkID = d3.select('svg').selectAll('.link');
 			
 			for (let i = 0; i < nodeID[0].length; i++){
-				nodeID[0][i].style.fill = "#FF5349";
 				nodeID[0][i].style.strokeWidth = "1.5px";
 				nodeID[0][i].style.r = "10";
 			}
@@ -120,29 +263,29 @@ function renderGraph() {
 				linkID[0][i].style.stroke = "#999";
 				linkID[0][i].style.strokeWidth = "1px";
 			}
-			
-			$('#details').text("Name: " + d.name + "\n" +   
-			                   "ID: " + d.id + "\n" + 
-			                   "Label: " + d.label + "\n" + 
-			                   "Weight: " + d.weight + "\n" +  
-			                   "Index: " + d.index + "\n\n");
+			//$('#details').text(String("Name: ").bold());
+      $('#details').text(" ")
+			$('#details').append(String("Name: ").bold() + d.name + "\n" +   
+			                   String("ID: ").bold()  + d.id + "\n" + 
+			                   String("Affiliation ID: " ).bold() + d.afid + "\n" + 
+			                   String("Weight: ").bold() + d.weight + "\n" +  
+			                   String("Index: ").bold() + d.index + "\n");
 
-			$('#details').append("CENTRALITIES \n");
+			$('#details').append("<hr>")
+			$('#details').append(String("Degree Centrality: ").bold()  + d.d_cen + "\n" +   
+			                   String("Betweenness Centrality: ").bold()  + d.b_cen.toFixed(5) + "\n" + 
+			                   String("Closeness Centrality: ").bold() + d.c_cen.toFixed(5) + "\n" + 
+			                   String("Eigenvector Centrality: ").bold() + d.e_cen.toFixed(5) + "\n\n" +  
+			                   String("Louvain Community ID: ").bold() + d.l_com);
 			
-			$('#details').append("Degree Centrality: " + d.d_cen + "\n" +   
-			                   "Betweenness Centrality: " + d.b_cen + "\n" + 
-			                   "Closeness Centrality: " + d.c_cen + "\n" + 
-			                   "Eigenvector Centrality: " + d.e_cen + "\n\n" +  
-			                   "Louvain Community: " + d.l_com);
-			
-			//var zoom_handler = d3.behavior.zoom().on("zoom", zoom_actions);
+			var zoom_handler = d3.behavior.zoom().on("zoom", zoom_actions);
 			//var zoom_handler = d3.zoom().on("zoom", function() { 
             //   svg.attr("transform", d3.event.transform);
            // });
 			
 			//svg.call(zoom_handler)
 			//	.call(zoom_handler.transform, d3.zoomIdentity.translate(0, 0).scale(1));
-			
+
 			var current_node = this;
 			var largest = null,
 			weight = 0;
@@ -161,7 +304,6 @@ function renderGraph() {
 
 		  if (largest) {
 			d3.select(largest.node)
-			  .style("fill", "yellow")
 			  .style("stroke-width", 3)
 			  .style('r', "15");
 			largest.links.each(function() {
@@ -203,7 +345,7 @@ function renderGraph() {
           return d.y;
         });
       });
-	  
+	  /*
 	  var simulation = d3.forceSimulation(node)
 		.force("link", d3.forceLink().distance(300).id(function(d) {
 		  return d.id;
@@ -216,8 +358,7 @@ function renderGraph() {
 		.on("tick", ticked);
 
 	  simulation.force("link")
-		.links(graph.links);
-    });
+		.links(graph.links);*/
 }
 
 function zoom_actions() {
